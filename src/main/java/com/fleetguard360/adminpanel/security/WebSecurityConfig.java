ccb3cc9b58a79d3943589ba95ba31950153a77ec
PCC <alcides.puerta@udea.edu.co>
2025-06-25 09:42:30 -0500

    correccion jwt

--- src/main/java/com/fleetguard360/adminpanel/security/WebSecurityConfig.java
+++ src/main/java/com/fleetguard360/adminpanel/security/WebSecurityConfig.java
@@ -60,6 +60,7 @@ public class WebSecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/**").permitAll()
                                 .requestMatchers("/api/fleets/**").permitAll()
                                 .requestMatchers("/h2-console/**").permitAll()
                                 .requestMatchers("/swagger-ui/**").permitAll()
